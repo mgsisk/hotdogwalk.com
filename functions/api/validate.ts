347addaf2b75388b3c7f4997d47e4a2b7b950565
Michael Sisk <me@mgsisk.com>
2025-02-27 09:09:27 -0500

    Update validate.ts

--- functions/api/validate.ts
+++ functions/api/validate.ts
@@ -13,6 +13,17 @@ export interface Env {
   shirtxxx: string;
 }
 
+type Color = "r" | "o" | "b" | "p";
+type Ticket = "combo" | "event" | "voucher" | "shirt";
+type ShirtSize =
+  | "shirts"
+  | "shirtm"
+  | "shirtl"
+  | "shirtx"
+  | "shirtxx"
+  | "shirtxxx";
+type Size = "s" | "m" | "l" | "x" | "xx" | "xxx";
+
 export const onRequestPost: PagesFunction<Env> = async (context) => {
   const formData = await context.request.formData();
   const fees = {
@@ -29,22 +40,24 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
   };
   const output = {
     error: 0,
-    total: parseInt(formData.get("donation")),
+    total: parseInt(formData.get("donation") as string),
   };
 
   if (!formData.get("free2025")) {
-    output.total += fees[formData.get("ticket")];
+    output.total += fees[formData.get("ticket") as Ticket];
   }
 
   for (const field of formData.entries()) {
-    if (!field[0].match(/^shirt(s|m|l|x|xx|xxx)\-(r|o|b|p)$/)) {
+    if (!field[0].match(/^shirt(s|m|l|x|xx|xxx)-(r|o|b|p)$/)) {
       continue;
     }
 
-    output.total += parseInt(field[1]) * fees[field[0].split("-").shift()];
+    output.total +=
+      parseInt(field[1] as string) *
+      fees[field[0].split("-").shift() as ShirtSize];
   }
 
-  if (["voucher", "shirt"].indexOf(formData.get("ticket")) > -1) {
+  if (["voucher", "shirt"].indexOf(formData.get("ticket") as Ticket) > -1) {
     output.total -= fees.shirt;
   }
 
@@ -53,47 +66,59 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
   }
 
   if (
-    ["shirt", "voucher", "event", "combo"].indexOf(formData.get("ticket")) ===
-    -1
-  ) {
-    output.error++;
-  }
-
-  if (!formData.get("fname") || formData.get("fname").length > 255) {
-    output.error++;
-  }
-
-  if (!formData.get("lname") || formData.get("lname").length > 255) {
-    output.error++;
-  }
-
-  if (!formData.get("email") || formData.get("email").length > 255) {
-    output.error++;
-  }
-
-  if (formData.get("tel").replace(/\D+/g, "").length < 10) {
-    output.error++;
-  }
-
-  if (
-    ["combo", "event"].indexOf(formData.get("ticket")) > -1 &&
-    ["r", "o", "b", "p"].indexOf(formData.get("shirtc")) === -1
+    ["shirt", "voucher", "event", "combo"].indexOf(
+      formData.get("ticket") as Ticket,
+    ) === -1
   ) {
     output.error++;
   }
 
   if (
-    ["combo", "event"].indexOf(formData.get("ticket")) > -1 &&
-    ["s", "m", "l", "x", "xx", "xxx"].indexOf(formData.get("shirts")) === -1
+    !formData.get("fname") ||
+    (formData.get("fname") as string).length > 255
   ) {
     output.error++;
   }
 
-  if (["shirt", "voucher"].indexOf(formData.get("ticket")) > -1) {
+  if (
+    !formData.get("lname") ||
+    (formData.get("lname") as string).length > 255
+  ) {
+    output.error++;
+  }
+
+  if (
+    !formData.get("email") ||
+    (formData.get("email") as string).length > 255
+  ) {
+    output.error++;
+  }
+
+  if ((formData.get("tel") as string).replace(/\D+/g, "").length < 10) {
+    output.error++;
+  }
+
+  if (
+    ["combo", "event"].indexOf(formData.get("ticket") as Ticket) > -1 &&
+    ["r", "o", "b", "p"].indexOf(formData.get("shirtc") as Color) === -1
+  ) {
+    output.error++;
+  }
+
+  if (
+    ["combo", "event"].indexOf(formData.get("ticket") as Ticket) > -1 &&
+    ["s", "m", "l", "x", "xx", "xxx"].indexOf(
+      formData.get("shirts") as Size,
+    ) === -1
+  ) {
+    output.error++;
+  }
+
+  if (["shirt", "voucher"].indexOf(formData.get("ticket") as Ticket) > -1) {
     let minShirt = 1;
 
     for (const field of formData.entries()) {
-      if (field[0].match(/^shirt(s|m|l|x|xx|xxx)\-(r|o|b|p)$/)) {
+      if (field[0].match(/^shirt(s|m|l|x|xx|xxx)-(r|o|b|p)$/)) {
         minShirt = 0;
         break;
       }
@@ -102,7 +127,7 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
     output.error += minShirt;
   }
 
-  if (parseInt(formData.get("donation")) < 0) {
+  if (parseInt(formData.get("donation") as string) < 0) {
     output.error++;
   }
 
