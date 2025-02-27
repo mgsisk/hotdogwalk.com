db3c15db12fb61397021ddc7d98b67b2cbb59401
Michael Sisk <me@mgsisk.com>
2025-02-27 09:04:27 -0500

    Update [id].ts

--- functions/api/success/[id].ts
+++ functions/api/success/[id].ts
@@ -6,9 +6,38 @@ export interface Env {
   resend_key: string;
 }
 
+type Walker = {
+  id: number;
+  created: string;
+  updated: string;
+  ticket: "combo" | "event" | "voucher" | "shirt";
+  access: "super" | "vip" | "comp" | "general";
+  fname: string;
+  lname: string;
+  email: string;
+  tel: string;
+  shirts: "s" | "m" | "l" | "x" | "xx" | "xxx";
+  shirtc: "r" | "o" | "b" | "p";
+  price: number;
+  donation: number;
+  total: number;
+  payment: string;
+};
+
+type Merch = {
+  id: number;
+  walker: number;
+  type: string;
+  size: "s" | "m" | "l" | "x" | "xx" | "xxx";
+  color: "r" | "o" | "b" | "p";
+  price: number;
+  quantity: number;
+  total: number;
+};
+
 const sendMail = async (
-  walker: Record<string, unknown>,
-  merch: Record<string, unknown>[],
+  walker: Walker,
+  merch: Merch[],
   resend: Resend,
 ): Promise<boolean> => {
   try {
@@ -37,7 +66,7 @@ const sendMail = async (
 
     message += `<p>Thank you for registering for the 2025 Kalamazoo Hot Dog Walk!</p>`;
 
-    if (["shirt", "voucher"].indexOf(walker.ticket as string) > -1) {
+    if (["shirt", "voucher"].indexOf(walker.ticket) > -1) {
       message +=
         "<p>Pickup begins at noon on Thursday, May 22 at <a href='https://maps.app.goo.gl/J2pQm6g6UECT8dPY7'>Kalamazoo Loaves and Fishes</a> or any day after during their normal business hours.</p>";
     } else {
@@ -65,18 +94,18 @@ const sendMail = async (
     message += "<hr><h1>Registration Info</h1><table>";
     message += `<tr><th style="text-align:left">Name</th><td>${walker.fname} ${walker.lname}</td></tr>`;
     message += `<tr><th style="text-align:left">Email</th><td><a href="mailto:${walker.email}">${walker.email}</a></td></tr>`;
-    message += `<tr><th style="text-align:left">Phone</th><td><a href="tel:${walker.tel}">${(walker.tel as string).replace(/(\d{3})(\d{3})(\d{4})$/, "($1) $2-$3")}</a></td></tr>`;
-    message += `<tr><th style="text-align:left">Registered For</th><td>${tickets[walker.ticket as string]}</td></tr>`;
-    message += `<tr><th style="text-align:left">Registered On</th><td>${new Date(walker.created as string).toLocaleDateString("en-US")}</td></tr>`;
+    message += `<tr><th style="text-align:left">Phone</th><td><a href="tel:${walker.tel}">${walker.tel.replace(/(\d{3})(\d{3})(\d{4})$/, "($1) $2-$3")}</a></td></tr>`;
+    message += `<tr><th style="text-align:left">Registered For</th><td>${tickets[walker.ticket]}</td></tr>`;
+    message += `<tr><th style="text-align:left">Registered On</th><td>${new Date(walker.created).toLocaleDateString("en-US")}</td></tr>`;
     message += "</table>";
 
-    if (["event", "combo"].indexOf(walker.ticket as string) > -1) {
+    if (["event", "combo"].indexOf(walker.ticket) > -1) {
       message += "<h2>Dogs</h2>";
       message +=
         "<p>Your registration includes one hot dog at each stop chosen from a selection of the best dogs each partner restaurant has to offer, or you can request a custom dog at any stop during the event if none of the prepared dogs work for you.</p>";
     }
 
-    if (["voucher", "combo"].indexOf(walker.ticket as string) > -1) {
+    if (["voucher", "combo"].indexOf(walker.ticket) > -1) {
       message += "<h2>Voucher Book</h2>";
       message +=
         "<p>Your registration includes a voucher book good for one hot dog at each partner restaurant, redeemable throughout the year.</p>";
@@ -84,30 +113,30 @@ const sendMail = async (
 
     message += "<h2>Shirts</h2><table>";
 
-    if (["event", "combo"].indexOf(walker.ticket as string) > -1) {
-      message += `<tr><th style="text-align:left">For the Walk</th><td>${sizes[walker.shirts as string]}</td><td>${colors[walker.shirtc as string]}</td></tr>`;
+    if (["event", "combo"].indexOf(walker.ticket) > -1) {
+      message += `<tr><th style="text-align:left">For the Walk</th><td>${sizes[walker.shirts]}</td><td>${colors[walker.shirtc]}</td></tr>`;
     }
 
     merch.forEach((shirt) => {
-      message += `<tr><th style="text-align:left">${shirt.quantity}</th><td>${sizes[shirt.size as string]}</td><td>${colors[shirt.color as string]}</td></tr>`;
+      message += `<tr><th style="text-align:left">${shirt.quantity}</th><td>${sizes[shirt.size]}</td><td>${colors[shirt.color]}</td></tr>`;
     });
 
     message += "</table>";
 
     if (walker.donation) {
-      message += `<h2>Donation</h2><p>Thank you for your \$${walker.donation} to Kalamazoo Loaves &amp; Fishes!</p>`;
+      message += `<h2>Donation</h2><p>Thank you for your $${walker.donation} to Kalamazoo Loaves &amp; Fishes!</p>`;
     }
 
     const send = await resend.emails.send({
       from: "Kalamazoo Hot Dog Walk <info@hotdogwalk.com>",
-      to: walker.email as string,
+      to: walker.email,
       subject: "Registration Complete",
       html: message,
     });
 
     return !send.error;
-  } catch (e: any) {
-    return false;
+  } catch (e) {
+    return e ? false : false;
   }
 };
 
@@ -116,11 +145,11 @@ export const onRequestGet: PagesFunction<Env> = async (
 ): Promise<Response> => {
   let sent = true;
   const year = context.env.open.split("-").shift();
-  const walker = await context.env.DB.prepare(
+  const walker = (await context.env.DB.prepare(
     `select * from walk${year} where id = ?`,
   )
     .bind(context.params.id)
-    .first();
+    .first()) as Walker;
 
   if (walker && !walker.updated) {
     const { results = [] } = await context.env.DB.prepare(
@@ -129,7 +158,11 @@ export const onRequestGet: PagesFunction<Env> = async (
       .bind(context.params.id)
       .all();
 
-    sent = await sendMail(walker, results, new Resend(context.env.resend_key));
+    sent = await sendMail(
+      walker,
+      results as unknown as Merch[],
+      new Resend(context.env.resend_key),
+    );
 
     if (sent) {
       await context.env.DB.prepare(
